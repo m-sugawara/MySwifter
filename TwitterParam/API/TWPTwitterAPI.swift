f91a44df8c1e90195b7153187ed225c446a4b3f7
m-sugawara <masa.sugawar@gmail.com>
2019-11-17 05:35:44 +0900

    WIP: fix unfavoriteTweet API

--- TwitterParam/API/TWPTwitterAPI.swift
+++ TwitterParam/API/TWPTwitterAPI.swift
@@ -466,21 +466,24 @@ final class TWPTwitterAPI: NSObject {
         }
     }
     
-    func postDestroyFavoriteWithID(id: String, includeEntities: Bool? = nil) -> RACSignal? {
-        
-        return RACSignal.createSignal({ (subscriber) -> RACDisposable! in
-            self.swifter.postDestroyFavoriteWithID(id,
+    func postDestroyFavorite(with id: String, includeEntities: Bool? = nil, tweetMode: TweetMode = .default) -> SignalProducer<Void, Error> {
+
+        return SignalProducer<Void, Error> { observer, lifetime in
+            guard !lifetime.hasEnded else {
+                observer.sendInterrupted()
+                return
+            }
+            self.swifter.unfavoriteTweet(
+                forID: id,
                 includeEntities: includeEntities,
-                success: { (status) -> Void in
-                    subscriber.sendNext(nil)
-                    subscriber.sendCompleted()
-            }, failure: { (error) -> Void in
-                subscriber.sendError(error)
-            })
-            
-            return RACDisposable(block: { () -> Void in
-            })
-        })
+                tweetMode: tweetMode,
+                success: { json in
+                    observer.send(value: ())
+                    observer.sendCompleted()
+            }) { error in
+                observer.send(error: error)
+            }
+        }
     }
     
     // MARK: - Wrapper Methods(follow)

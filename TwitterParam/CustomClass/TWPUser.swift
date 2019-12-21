f58f44d8b2e6999e6d46159334310ffc3211d13a
m-sugawara <masa.sugawar@gmail.com>
2019-12-22 01:45:55 +0900

    add userInfoModel tests

--- TwitterParam/CustomClass/TWPUser.swift
+++ TwitterParam/CustomClass/TWPUser.swift
@@ -12,26 +12,24 @@ import SwifteriOS
 struct TWPUser {
     let userId: String
     let name: String?
-    let screenName: String?
-    let screenNameWithAt: String?
+    let screenName: String
     let profileImageUrl: URL?
     var following: Bool
     let friendsCount: Int
     let followersCount: Int
 
     init(userId: String,
-         name: String?,
-         screenName: String?,
-         profileImageUrl: String?,
-         following: Bool,
-         friendsCount: Int,
-         followersCount: Int
+         name: String = "",
+         screenName: String = "",
+         profileImageUrl: String = "",
+         following: Bool = false,
+         friendsCount: Int = 0,
+         followersCount: Int = 0
     ) {
         self.userId = userId
         self.name = name
         self.screenName = screenName
-        self.screenNameWithAt = (screenName != nil) ? "@" + screenName! : ""
-        self.profileImageUrl = URL(string: profileImageUrl!)
+        self.profileImageUrl = URL(string: profileImageUrl)
         self.following = following
         self.friendsCount = friendsCount
         self.followersCount = followersCount
@@ -40,13 +38,17 @@ struct TWPUser {
     init(dictionary: [String: JSON]) {
         self.init(
             userId:          dictionary["id_str"]?.string ?? "",
-            name:            dictionary["name"]?.string,
-            screenName:      dictionary["screen_name"]?.string,
-            profileImageUrl: dictionary["profile_image_url"]?.string,
+            name:            dictionary["name"]?.string ?? "",
+            screenName:      dictionary["screen_name"]?.string ?? "",
+            profileImageUrl: dictionary["profile_image_url"]?.string ?? "",
             following:       dictionary["following"]?.bool ?? false,
             friendsCount:    dictionary["friends_count"]?.integer ?? 0,
             followersCount:  dictionary["followers_count"]?.integer ?? 0
         )
+    }
+
+    var screenNameWithAt: String {
+        return (screenName.isEmpty) ? "" : "@" + screenName
     }
 
     var profileImageUrlString : String? {
---
+++ TwitterParamTests/ViewModel/TWPUserInfoViewModelTests.swift
@@ -0,0 +1,128 @@
+//
+//  TWPUserInfoViewModelTests.swift
+//  TwitterParamTests
+//
+//  Created by M_Sugawara on 2019/12/22.
+//  Copyright © 2019 sugawar. All rights reserved.
+//
+
+import XCTest
+
+@testable import TwitterParam
+
+class TWPUserInfoViewModelTests: XCTestCase {
+
+    private var viewModel: TWPUserInfoViewModel!
+
+    override func setUp() {
+        viewModel = TWPUserInfoViewModel()
+    }
+
+    override func tearDown() {
+        viewModel = nil
+    }
+
+    func testGetUserAction() {
+        let expectation = XCTestExpectation()
+
+        viewModel.getUserAction.apply().startWithResult { result in
+            switch result {
+            case .success:
+                XCTAssertTrue(false)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            }
+        }
+
+        wait(for: [expectation], timeout: 2.0)
+    }
+
+    func testGetUserTimeListAction() {
+        let expectation = XCTestExpectation()
+
+        viewModel.getUserTimeLineAction.apply().startWithResult { result in
+            switch result {
+            case .success:
+                XCTAssertTrue(false)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            }
+        }
+
+        wait(for: [expectation], timeout: 2.0)
+    }
+
+    func testGetUserImageListAction() {
+        let expectation = XCTestExpectation()
+
+        viewModel.getUserImageListAction.apply().startWithResult { result in
+            switch result {
+            case .success:
+                XCTAssertEqual(self.viewModel.tweets.count, 1)
+                expectation.fulfill()
+            case .failure:
+                XCTAssertTrue(false)
+                expectation.fulfill()
+            }
+        }
+
+        wait(for: [expectation], timeout: 2.0)
+    }
+
+    func testGetUserFavoritesAction() {
+        let expectation = XCTestExpectation()
+
+        viewModel.getUserFavoritesAction.apply().startWithResult { result in
+            switch result {
+            case .success:
+                XCTAssertTrue(false)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            }
+        }
+
+        wait(for: [expectation], timeout: 2.0)
+    }
+
+    func testFollowAction() {
+        let expectation = XCTestExpectation()
+
+        viewModel.followAction.apply().startWithResult { result in
+            switch result {
+            case .success:
+                XCTAssertTrue(false)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            }
+        }
+
+        wait(for: [expectation], timeout: 2.0)
+    }
+
+    func testUnfollowAction() {
+        let expectation = XCTestExpectation()
+
+        viewModel.user = TWPUser(userId: "", following: true)
+        viewModel.followAction.apply().startWithResult { result in
+            switch result {
+            case .success:
+                XCTAssertTrue(false)
+                expectation.fulfill()
+            case .failure(let error):
+                XCTAssertNotNil(error)
+                expectation.fulfill()
+            }
+        }
+
+        wait(for: [expectation], timeout: 2.0)
+    }
+
+}
